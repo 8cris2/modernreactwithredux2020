8bcc6e1ab2eb9a45eaa6a252192b5c22ec25aa49
Crispian <crispian.anyster@gmail.com>
2020-08-26 10:00:47 +0200

    1. Move App.js to root of components folder. 2. Create searchbar.js base component. 3. Link searchbar component to the App.js file

---
+++ pics/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar/SearchBar';
+
+// functional app component
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
--- pics/src/components/App/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// functional app component
-const App = () => {
-  return <div> App </div>;
-};
-
-export default App;
