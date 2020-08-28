b56d897215472f5e18c93d71f6c16e089dbb26af
Crispian <crispian.anyster@gmail.com>
2020-08-28 09:45:22 +0200

    1. Add method to detect user input on an input field 2. Use onChange event handler on input field and pass the callback in on the onChange event.

--- pics/src/components/SearchBar/SearchBar.js
+++ pics/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    render() {
-      return (
-        <div className="ui segment">
-          <form className="ui form">
-          <label style={{ marginBottom: '5px' }}> Image Search </label>
-            <input className="field" type = "text" />
-          </form>
-        </div>)
-      }
+
+  // method for detecting user input
+  onInputChange(event) {
+    console.log('Event: ', event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+        <label style={{ marginBottom: '5px' }}> Image Search </label>
+
+        {/*
+            { this.onInputChange } is a callback function in the
+            onchange event handler leaving out the parenthesis.
+            using the parethesis causes the function to fire off
+            on every component render to the browser.
+        */}
+
+          <input className="field" type="text" onChange={ this.onInputChange } />
+        </form>
+      </div>)
     }
+  }
 
 export default SearchBar;
