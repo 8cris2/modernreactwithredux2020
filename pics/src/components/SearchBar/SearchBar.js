5820a357ea17bc4b7ff3277ed013caba875afeaa
Crispian <crispian.anyster@gmail.com>
2020-08-28 09:23:47 +0200

    1. Use semantic-ui as the UI framework for UI components. 2. Add a form input field. 3. Style the form input field using semantic-ui available classes.

--- pics/src/components/SearchBar/SearchBar.js
+++ pics/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  render() {
-    return <div> Search Bar </div>;
-  }
-}
+    render() {
+      return (
+        <div className="ui segment">
+          <form className="ui form">
+          <label style={{ marginBottom: '5px' }}> Image Search </label>
+            <input className="field" type = "text" />
+          </form>
+        </div>)
+      }
+    }
 
 export default SearchBar;
