52de86a4ef9ed84f2edb47e4c4faa4c0525b8e8c
Crispian <crispian.anyster@gmail.com>
2020-08-31 19:28:44 +0200

    1. Handling form submission 2. Arrow function example.

--- pics/src/components/SearchBar/SearchBar.js
+++ pics/src/components/SearchBar/SearchBar.js
@@ -2,33 +2,26 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-  // method for detecting user input
-  onInputChange(event) {
-    console.log('Event: ', event.target.value);
+  // initialise state object
+  state = { term: '' };
+
+  onFormSubmit = (event) => {
+    
+    event.preventDefault();
+    console.log('Term: ', this.state.term);
+
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={ this.onFormSubmit } className="ui form">
         <label style={{ marginBottom: '5px' }}> Image Search </label>
 
-        {/*
-            { this.onInputChange } is a callback function in the
-            onchange event handler leaving out the parenthesis.
-            using the parethesis causes the function to fire off
-            on every component render to the browser.
-        */}
-
-          {/* <input className="field" type="text" onChange={ this.onInputChange } /> */}
-
-          {/* use alternate syntax to achieve the same result but using an arrow function
-              declaring the event param and passing the event.target.value in the code block
-
-              e abbr instead of event.
-          */}
-
-          <input className="field" type="text" onChange={ e => { console.log(e.target.value) } } />
+          <input className="field"
+                type="text"
+                value={ this.state.term }
+                onChange={ (e) => this.setState({ term: e.target.value }) } />
         </form>
       </div>)
     }
